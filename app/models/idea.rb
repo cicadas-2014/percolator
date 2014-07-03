21e79119c636f840b4768b360c124ea8ce8e014c
Simon Says... push to master <smxblah@gmail.com>
2014-07-03 16:37:46 -0400

    remove duplicate files

--- app/models/idea.rb
+++
@@ -1,9 +0,0 @@
-class Problem < ActiveRecord::Base
-  has_many :solutions
-  has_many :votes, as: :voteable
-  belongs_to :user
-
-  def voteables
-    Vote.where(voteable_type: "problem", voteable_id: self.id)
-  end
-end
