34ac7ef10eefa6de5b160e86392c30f1126f0c2d
Tim <timothyjmcclung@gmail.com>
2014-07-03 16:42:08 -0400

    devise spike, old routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root 'user#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
