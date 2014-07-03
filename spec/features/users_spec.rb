f038c7d949123c173a100e93af89fa846e1cc832
Tim <timothyjmcclung@gmail.com>
2014-07-03 19:40:00 -0400

    accidentally rolled back users features

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -10,6 +10,26 @@ feature 'Users' do
 		expect(current_path).to eq(root_path)
 	end
 
+	scenario 'Password must be 8 characters or more' do
+		visit new_user_registration_path
+		fill_in 'user_email', with: 'user2@dbc.gov'
+		fill_in 'user_password', with: 'pass'
+		fill_in 'user_password_confirmation', with: 'pass'
+		click_button 'Sign up'
+		expect(page.body).to have_content("Password is too short (minimum is 8 characters)")
+	end
+
+	scenario 'User cannot create duplicate email' do
+		User.create( email: 'user@dbc.gov', password: 'password')
+		visit new_user_registration_path
+		fill_in 'user_email', with: 'user@dbc.gov'
+		fill_in 'user_password', with: 'password'
+		fill_in 'user_password_confirmation', with: 'password'
+		click_button 'Sign up'
+		expect(page).to have_content("Email has already been taken")
+	end
+
+
 	let (:user) { User.create( email: 'user@dbc.gov', password: 'password')}
 	scenario 'User can login with valid information' do
 		visit new_user_session_path
@@ -17,5 +37,13 @@ feature 'Users' do
 		fill_in 'user_password', with: user.password
 		click_button 'Sign in'
 		expect(current_path).to eq(root_path)
-	end	
+	end
+
+	scenario 'User cannot login with invalid information' do
+		visit new_user_session_path
+		fill_in 'user_email', with: 'test@test.gov'
+		fill_in 'user_password', with: 'incorrect'
+		click_button 'Sign in'
+		expect(current_path).to eq(new_user_session_path)
+	end
 end
