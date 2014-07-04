e5dd3861caecd4b34445caf768de1abd22edd260
Simon Says... push to master <smxblah@gmail.com>
2014-07-04 11:38:46 -0400

    take out email column to make devise compatible

--- db/migrate/20140703011710_create_users.rb
+++ db/migrate/20140703011710_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
+      # t.string :email #take this out for devise
       t.string :password_digest
       t.integer :reputation
 
