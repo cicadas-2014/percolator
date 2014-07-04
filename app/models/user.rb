3e6acd726545425db147bcd2addd6451d2c937e7
Tim <timothyjmcclung@gmail.com>
2014-07-04 12:00:10 -0400

    overwriting user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  has_secure_password
-
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # has_many :interests
