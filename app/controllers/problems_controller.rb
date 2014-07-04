4e2afdfec5ebdd4eed6eaca03d679947e04d36c8
Simon Says... push to master <smxblah@gmail.com>
2014-07-04 11:34:01 -0400

    add testing for show/new

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,3 +1,22 @@
-class ProblemsController < ApplicationController::Base
+class ProblemsController < ApplicationController
   protect_from_forgery
+  before_action :load_user, only: :create
+
+  def index
+    @problems = Problem.all
+  end
+
+  def show
+    @problem = Problem.find params[:id]
+  end
+
+  def new
+    @problem = Problem.new
+  end
+
+  private
+
+  def load_user
+    @user = User.find(params[:user_id])
+  end
 end
