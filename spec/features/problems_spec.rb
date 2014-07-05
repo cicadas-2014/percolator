f0378632ade9d6623e9e3e36b8c1296bce5fd873
Simon Says... push to master <smxblah@gmail.com>
2014-07-05 10:48:41 -0400

    change should_have from css to content for invalid entries to look for flash message

--- spec/features/problems_spec.rb
+++ spec/features/problems_spec.rb
@@ -1,37 +1,32 @@
 require 'spec_helper'
 
 feature 'User creates a new problem', type: :feature, js: true do
-  # # let!(:user) { create :user }
-  # let!(:problem) { create :problem }
-  # title = "Curbing sensationalism in the media"
-  # description = "There is tendency for the press to play up and dwell on stories that
-  # are sensational - murders, car crashes, kidnappings, sex scandals and the like."
+  let!(:problem) { create :problem }
 
-  # scenario 'with valid title and description' do
-  #   create_problem_with("holla back", "nigga")
-  #   expect(page).to have_content("Indigo")
-  # end
+  scenario 'with valid title and description' do
+    create_problem_with("holla back", "homie")
+    expect(page).to have_css("div#canvas_container")
+  end
 
-  # # scenario 'with invalid title' do
-  # #   create_problem_with(nil, description)
+  scenario 'with invalid title' do
+    create_problem_with(nil, "description1")
 
-  # #   expect(page).to have_content("First argument in form cannot contain nil or be empty")
-  # # end
+    expect(page).to have_content("All fields must be populated!")
+  end
 
-  # # scenario 'with invalid description' do
-  # #   create_problem_with(title, nil)
+  scenario 'with invalid description' do
+    create_problem_with("title1", nil)
 
-  # #   expect(page).to have_content("First argument in form cannot contain nil or be empty")
-  # # end
+    expect(page).to have_content("All fields must be populated!")
+  end
 
-  # def create_problem_with(title, description)
-  #   visit new_problem_path
-  #   # click_link 'Posit Problem'
+  def create_problem_with(title, description)
+    visit new_problem_path
 
-  #   within("#new_problem") do
-  #     fill_in 'problem_title', with: title
-  #     fill_in 'problem_description', with: description
-  #   end
-  #   click_button 'Posit'
-  # end
+    within("#new_problem") do
+      fill_in 'problem_title', with: title
+      fill_in 'problem_description', with: description
+    end
+    click_button 'Posit'
+  end
 end
