1d9df74f28a75a6dd879c685345df4e2d6c3012b
Tim <timothyjmcclung@gmail.com>
2014-07-05 22:27:02 -0400

    added passing solution model specs

---
+++ spec/models/solution_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe Solution do
+  title = "Pass out bagels every day"
+  description = "Bagels have the highest density of nutrition out of any breakfast wheat product."
+  let!(:user) { create :user }
+  let!(:problem) { create :problem }
+
+  context "validations" do
+    it "is valid with a title and description" do
+      solution = create_solution_with(title, description, user, problem)
+      expect(solution).to be_valid
+    end
+
+    it "is invalid with an empty title" do
+      solution = create_solution_with(nil, description, user, problem)
+      expect(solution).to have(1).errors_on(:title)
+    end
+
+    it "is invalid with a duplicate title" do
+      solution = create_solution_with(title, description, user, problem)
+      expect(Solution.new(title: title, description: description, user: user, problem: problem)).to have(1).errors_on(:title)
+    end
+
+    it "is invalid with an empty description" do
+      solution = create_solution_with(title, nil, user, problem)
+      expect(solution).to have(1).errors_on(:description)
+    end
+
+    it "is invalid without a user association" do
+      expect(Solution.create(title: title, description: description, user_id: nil, problem: problem)).to have(1).errors_on(:user_id)
+    end
+  end
+
+  def create_solution_with(title, description, user, problem)
+    Solution.create(title: title, description: description, user_id: user.id, problem_id: problem.id)
+  end
+end
