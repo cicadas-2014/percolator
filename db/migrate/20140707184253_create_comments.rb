4d26c7ea613b7fff25cc93fcec990f469efe98ea
Simon Says... push to master <smxblah@gmail.com>
2014-07-07 19:44:24 -0400

    add username column to comments table

--- db/migrate/20140707184253_create_comments.rb
+++ db/migrate/20140707184253_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.references :commentable, polymorphic: true
       t.references :user
+      t.string :username
 
       t.timestamps
     end
