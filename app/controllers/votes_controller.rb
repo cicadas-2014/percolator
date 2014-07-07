908658c64fdc3475b6be9b3eca1a15a74ff43ab7
Simon Says... push to master <smxblah@gmail.com>
2014-07-07 04:22:51 -0400

    -add controller methods for problem upvote/downvote -replace hard-coded problem # with data sent from ajax post based on dom element that triggered the click event (problem/solution)

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,6 @@
 class VotesController < ApplicationController
-  def upvote
-    solution = Solution.find 10
+  def solution_upvote
+    solution = Solution.find params[:solution_number]
     upvote = solution.voteables.find_by(user_id: current_user.id, vote_type: true)
     downvote = solution.voteables.find_by(user_id: current_user.id, vote_type: false)
     if !upvote
@@ -9,14 +9,14 @@ class VotesController < ApplicationController
       end
       solution.voteables.create(user_id: current_user.id, vote_type: true)
     end
-      @vote_count = [solution.voteables.where(vote_type: true).count, solution.voteables.where(vote_type: false).count]
-  respond_to do |format|
-    format.js {render json: @vote_count}
-  end
+    @vote_count = [solution.voteables.where(vote_type: true).count, solution.voteables.where(vote_type: false).count]
+    respond_to do |format|
+      format.js {render json: @vote_count}
+    end
   end
 
-  def downvote
-    solution = Solution.find 10
+  def solution_downvote
+    solution = Solution.find params[:solution_number]
     upvote = solution.voteables.find_by(user_id: current_user.id, vote_type: true)
     downvote = solution.voteables.find_by(user_id: current_user.id, vote_type: false)
     if !downvote
@@ -25,10 +25,41 @@ class VotesController < ApplicationController
       end
       solution.voteables.create(user_id: current_user.id, vote_type: false)
     end
-      @vote_count = [solution.voteables.where(vote_type: true).count, solution.voteables.where(vote_type: false).count]
+    @vote_count = [solution.voteables.where(vote_type: true).count, solution.voteables.where(vote_type: false).count]
+    respond_to do |format|
+      format.js {render json: @vote_count}
+    end
+  end
 
- respond_to do |format|
-    format.js {render json: @vote_count}
+  def problem_upvote
+    problem = Problem.find params[:problem_number]
+    upvote = problem.voteables.find_by(user_id: current_user.id, vote_type: true)
+    downvote = problem.voteables.find_by(user_id: current_user.id, vote_type: false)
+    if !upvote
+      if downvote
+        downvote.destroy
+      end
+      problem.voteables.create(user_id: current_user.id, vote_type: true)
+    end
+    @vote_count = [problem.voteables.where(vote_type: true).count, problem.voteables.where(vote_type: false).count]
+    respond_to do |format|
+      format.js {render json: @vote_count}
+    end
+  end
+
+  def problem_downvote
+    problem = Problem.find params[:problem_number]
+    upvote = problem.voteables.find_by(user_id: current_user.id, vote_type: true)
+    downvote = problem.voteables.find_by(user_id: current_user.id, vote_type: false)
+    if !downvote
+      if upvote
+        upvote.destroy
+      end
+      problem.voteables.create(user_id: current_user.id, vote_type: false)
+    end
+    @vote_count = [problem.voteables.where(vote_type: true).count, problem.voteables.where(vote_type: false).count]
+    respond_to do |format|
+      format.js {render json: @vote_count}
     end
   end
 end
