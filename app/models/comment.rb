bae3c01cacbe3c14194b7339418b0f66ddb2c0e9
Simon Says... push to master <smxblah@gmail.com>
2014-07-07 17:07:28 -0400

    add ajax method receivers for category specific comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,24 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  private
+
+  def problem_comments(problem_number)
+    @comments = {
+      prob_comment: Comment.where(commentable_type: "problem", commentable_id: problem_number)
+    }
+  end
+
+  def problem_solutions(problem_number, solution_number)
+    @comments = {
+      sol_comment: Comment.where(commentable_type: "solution", commentable_id: solution_number)
+    }
+  end
+
+  def problem_improvements(problem_number, improvement_number)
+    @comments = {
+      imp_comment: Comment.where(commentable_type: "improvement", commentable_id: improvement_number)
+    }
+  end
 end
