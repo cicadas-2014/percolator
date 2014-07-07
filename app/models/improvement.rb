6e8c0eb42871b008b76a85d985cb99f16a0b8243
Simon Says... push to master <smxblah@gmail.com>
2014-07-07 15:02:16 -0400

    create methods to access commentables in improvement/problem/solution

--- app/models/improvement.rb
+++ app/models/improvement.rb
@@ -7,8 +7,12 @@ class Improvement < ActiveRecord::Base
   validates :description, presence: true
   validates_presence_of :user_id
   validates_presence_of :solution_id
-  
+
   def voteables
     Vote.where(voteable_type: "improvement", voteable_id: self.id)
+  end
+
+  def commentables
+    Comment.where(commentable_type: "improvement", commentable_id: self.id)
   end
 end
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,13 +2,17 @@ class Problem < ActiveRecord::Base
   has_many :solutions
   has_many :votes, as: :voteable
   belongs_to :user
-  
+
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates_presence_of :user_id
-  
+
 
   def voteables
     Vote.where(voteable_type: "problem", voteable_id: self.id)
+  end
+
+  def commentables
+    Comment.where(commentable_type: "problem", commentable_id: self.id)
   end
 end
