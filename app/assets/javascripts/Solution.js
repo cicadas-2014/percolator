9f63c8340153a0a09342c85ece3d7ca6b783c039
Tim <timothyjmcclung@gmail.com>
2014-07-09 17:48:00 -0400

    opacity for solutions and better text wrapping

--- app/assets/javascripts/Solution.js
+++ app/assets/javascripts/Solution.js
@@ -8,8 +8,8 @@ function Solution(posX, posY, id, raphael) {
     this.upvotes = $.parseJSON(window.data).solutions[this.id].upvotes;
     this.downvotes = $.parseJSON(window.data).solutions[this.id].downvotes;
     this.radius = 10 + (this.upvotes + this.downvotes) / 2;
-    this.frameSprite = raphael.circle(posX, posY, this.radius + (this.radius * .1)).attr({fill: this.createVoteFrame(), stroke: 'none'});
-    this.sprite = raphael.circle(posX, posY, this.radius).attr({fill: '#6DA2FF', stroke: "none", XXX: "SIGNIFIER"});
+    this.frameSprite = raphael.circle(posX, posY, this.radius + (this.radius * .1)).attr({fill: this.createVoteFrame(), stroke: 'none', opacity: .3});
+    this.sprite = raphael.circle(posX, posY, this.radius).attr({fill: '#6DA2FF', stroke: "none", XXX: "SIGNIFIER", opacity: .3});
     this.sprite.node.id = id;
     this.textSprite = undefined;
     this.createText();
@@ -29,7 +29,7 @@ Solution.prototype.createText = function () {
     var tempText = "";
     for (var i = 0; i < words.length; i++) {
         this.textSprite.attr("text", tempText + " " + words[i]);
-        if (this.textSprite.getBBox().width > (1.3 * this.radius)) {
+        if (this.textSprite.getBBox().width > (3.2 * this.radius)) {
             tempText += "\n" + words[i];
         } else {
             tempText += " " + words[i];
@@ -37,7 +37,7 @@ Solution.prototype.createText = function () {
     }
 
     this.textSprite.attr("text", tempText);
-    this.textSprite.attr({ "font-size": ((this.radius * .15) + 3), "font-family": "Opificio", "fill": "#FFFFFF"});
+    this.textSprite.attr({ "font-size": ((this.radius * .15) + 2), "font-family": "Opificio", "fill": "#FFFFFF"});
     this.textSprite.node.setAttribute("pointer-events", "none");
 };
 
@@ -54,9 +54,7 @@ Solution.prototype.animate = function (direction) { // only called during the in
 
 Solution.prototype.addEventListeners = function () {
     var delegate = this
-
     var target = delegate.original_id ? $("#" + this.original_id) : $('#' + this.id);
-
     target.bind({
         click: function () {
             if (!Percolator.isZooming) {
